dc5bc70b959f017bee15def15a329a94e62292e5
Manan Arora <mananarora313@gmail.com>
2024-07-07 17:24:40 -0500

    added java solution to 1768

---
+++ LeetCode 75/1768. Merge Strings Alternately/solutionJava.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+class solutionJava {
+
+    public static String mergeAlternately(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        String res = "";
+        int i = 0;
+        while (i < m && i < n) {
+            res = res + word1.charAt(i);
+            res = res + word2.charAt(i);
+            i++;
+        }
+        if(m<n){
+            String remaining = word2.substring(m, n);
+            res += remaining;
+        }
+        else if(n<m){
+            String remaining = word1.substring(n,m);
+            res += remaining;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String word1 = scn.next();
+        String word2 = scn.next();
+        System.out.println(mergeAlternately(word1, word2));
+        scn.close();
+    }
+}
