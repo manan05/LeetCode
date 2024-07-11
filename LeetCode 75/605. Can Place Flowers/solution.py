41d426f0c8c9127cec8503f9a2a121c1c0b6eba4
Manan Arora <mananarora313@gmail.com>
2024-07-10 21:55:31 -0500

    updated python solution 605

---
+++ LeetCode 75/605. Can Place Flowers/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def placeFlowers(flowerbed, n):
+        count = 0
+        for i in range(len(flowerbed)):
+            if(flowerbed[i] == 0):
+                if(len(flowerbed) == 1):
+                    count += 1
+                elif(i == 0):
+                    if(flowerbed[i+1] == 0):
+                        flowerbed[i] = 1
+                        count += 1
+                elif(i == len(flowerbed)-1):
+                    if(flowerbed[i-1] == 0):
+                        flowerbed[i] = 1
+                        count += 1
+                elif(flowerbed[i-1] == 0 and flowerbed[i+1] == 0):
+                    flowerbed[i] = 1
+                    count += 1
+        if(count >= n):
+            return True
+        else:
+            return False
+
+print("Size of array:")
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+print("Plants to place:")
+plants: int = int(input())
+print(Solution.placeFlowers(arr, plants))
