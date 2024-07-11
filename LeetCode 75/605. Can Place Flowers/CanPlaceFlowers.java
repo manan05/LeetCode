d5f9f9bb42b9f040546b6d92c26eaca78a38e5ea
Manan Arora <mananarorauta@gmail.com>
2024-07-10 23:32:41 -0500

    605 (#8)
    * updated python solution 605
    
    * updated 605 with java solution

---
+++ LeetCode 75/605. Can Place Flowers/CanPlaceFlowers.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+public class CanPlaceFlowers {
+
+    public static boolean CanPlaceFlowers(int[] arr, int n){
+        int m = arr.length;
+        int res = 0;
+        for(int i =0;i<m;i++){
+            if(arr[i] == 0){
+                if(m == 1){
+                    res++;
+                }
+                else if(i == 0){
+                    if(arr[i+1] == 0){
+                        arr[i] = 1;
+                        res ++;
+                    }
+                }
+                else if(i == m-1){
+                    if(arr[i-1] == 0){
+                        arr[i] = 1;
+                        res ++;
+                    }
+                }
+                else{
+                    if(arr[i+1] == 0 && arr[i -1] == 0){
+                        arr[i] = 1;
+                        res++;
+                    }
+                }
+            }
+        }
+        if(res >= n){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Size of flowerbed:");
+        int n = scn.nextInt();
+        int[] flowerbed = new int[n];
+        for(int i = 0; i<n; i++){
+            flowerbed[i] = scn.nextInt();
+        }
+        System.out.println("Extra Flowers?");
+        int z = scn.nextInt();
+        scn.close();
+        System.out.println(CanPlaceFlowers(flowerbed, z));
+    }
+}
