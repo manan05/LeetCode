68c859bae048f008daa01184a8e073d816ca5b42
Manan Arora <mananarora313@gmail.com>
2024-07-13 18:49:03 -0500

    updated with java solution

---
+++ LeetCode 75/238. Product of Array Except Self/productExceptSelf.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class productExceptSelf {
+    public static int[] productExceptSelfSol(int[] arr){
+        int n = arr.length;
+        int[] res = new int[n];
+        Arrays.fill(res, 1);
+
+        int pre = 1;
+        for(int i = 0; i< n; i++){
+            res[i] *= pre;
+            pre *= arr[i];
+        }
+
+        int post = 1;
+        for(int i = n-1; i>=0; i --){
+            res[i] *= post;
+            post *= arr[i];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n ; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        System.out.println(productExceptSelfSol(arr));
+    }
+}
