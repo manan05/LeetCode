26ceb72947c4c2f11430816e81c2634cc514cf02
Manan Arora <mananarora313@gmail.com>
2024-07-14 14:54:46 -0500

    updated with O(n) code in py

--- LeetCode 75/334. Increasing Triplet Subsequence/solution.py
+++ LeetCode 75/334. Increasing Triplet Subsequence/solution.py
@@ -14,8 +14,22 @@ class Solution:
                 if(i > j[1] and arr[i] > arr[j[1]]):
                     return True
         return False
+    
+    def incTripleSubsequenceOnWithoutSpace(arr):
+        first = second = float('inf')
+        first_idx = second_idx = -1
 
+        for i in range(len(arr)):
+            if(arr[i] <= first and i > first_idx):
+                first = arr[i]
+                first_idx = i
+            elif(arr[i] <= second and i > second_idx):
+                second = arr[i]
+                second_idx = i
+            else:
+                return True
+        return False
         
 
-arr = [1,5,0,4,1,3]
-print(Solution.incTripletSubsequence(arr))
+arr = [0,1,2,3,4]
+print(Solution.incTripleSubsequenceOnWithoutSpace(arr))
