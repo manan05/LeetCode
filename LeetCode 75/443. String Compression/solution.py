a5d6ff2efebfc023462b581f6c82f51bbda13783
Manan Arora <mananarorauta@gmail.com>
2024-07-16 22:47:58 -0500

    updated with complete python solution O(n) (#17)

--- LeetCode 75/443. String Compression/solution.py
+++ LeetCode 75/443. String Compression/solution.py
@@ -1,25 +1,24 @@
 class Solution:
-    # Using Hashmap or dict but we cannot (31/76) pass
-    def compress(chars):
-        hashmap = {}
-        for i in range(len(chars)):
-            if(chars[i] not in hashmap.keys()):
-               hashmap[chars[i]] = 1
-            else:
-                z = hashmap.get(chars[i]) + 1
-                hashmap[chars[i]] = z
+    def compress(self, chars: List[str]) -> int:
         s = ""
-        for i,j in hashmap.items():
-            s += i
-            if(j == 1):
-                pass
+        s+= chars[0]
+        i = 1
+        count = 1
+        while(count< len(chars)):
+            if(chars[count-1] == chars[count]):
+                i +=1
+                count +=1
             else:
-                s += str(j)
-        for i in range(len(s)):
-            chars[i] = s[i]
-        
-        return len(s)          
-
-chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
-
-print(Solution.compress(chars))
+                if(i != 1):
+                    s += str(i)
+                    i = 1
+                    s += chars[count]
+                    count +=1
+                else:
+                    s += chars[count]
+                    count +=1
+        if(i != 1):
+            s += str(i)
+        for j in range(len(s)):
+            chars[j] = s[j] 
+        return len(s)
