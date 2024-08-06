48147a48fd30c93017da1dd7709d21de16ac73ca
Manan Arora <mananarorauta@gmail.com>
2024-08-05 23:15:08 -0500

    added hashtable solution (#27)

--- LeetCode 75/Two Pointers/1679. Max no of K sum pairs/solution.py
+++ LeetCode 75/Two Pointers/1679. Max no of K sum pairs/solution.py
@@ -19,7 +19,24 @@ class Solution:
                 i += 1
         return count
 
+    def maxKSumPairsLinear(nums, k):
+        num_map = {}  # create an empty dictionary
+        count = 0
+
+        for num in nums:
+            res = k - num
+            if res in num_map and num_map[res] > 0:
+                count += 1
+                num_map[res] -= 1
+            else:
+                num_map[num] = num_map.get(res, 0) + 1
+
+        return count
+
+        print(dict)
+
 
 nums = [3, 1, 3, 4, 3]
 k = 6
-print(Solution.maxKSumPairs(nums, k))
+# print(Solution.maxKSumPairs(nums, k))
+print(Solution.maxKSumPairsLinear(nums, k))
