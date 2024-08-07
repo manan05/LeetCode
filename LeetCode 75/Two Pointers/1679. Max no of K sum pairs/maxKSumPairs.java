b9f1ef82909c558d5a21ce3eb08f451444254b88
Manan Arora <mananarora313@gmail.com>
2024-08-06 21:09:31 -0500

    updated with java code O(N)

--- LeetCode 75/Two Pointers/1679. Max no of K sum pairs/maxKSumPairs.java
+++ LeetCode 75/Two Pointers/1679. Max no of K sum pairs/maxKSumPairs.java
@@ -1,5 +1,7 @@
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 // Question link : https://leetcode.com/problems/max-number-of-k-sum-pairs/description/?envType=study-plan-v2&envId=leetcode-75
 // Companies: Amazon
@@ -25,6 +27,27 @@ public class maxKSumPairs {
             }
             else {
                 i ++;
+            }
+        }
+        return count;
+    }
+
+    public static int oNSolution(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int count = 0;
+        for(int i = 0; i< nums.length; i++) {
+            int res = k - nums[i];
+            if(map.containsKey(res)) {
+                count ++;
+                if(map.get(res) == 1) {
+                    map.remove(res);
+                }
+                else{
+                    map.put(res, map.get(res) -1);
+                }
+            }
+            else {
+                map.put(nums[i], map.getOrDefault(nums[i], 0) +1);
             }
         }
         return count;
