a1da6440a6ef1369e678be5830029d653036c723
Manan Arora <mananarora313@gmail.com>
2024-08-13 18:10:02 -0500

    initial commit for Question 1456

---
+++ LeetCode 75/Sliding Window/1456. MaxVowels SubStrings/solution.py
@@ -0,0 +1,11 @@
+# Question link: https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description/?envType=study-plan-v2&envId=leetcode-75
+# Companies: Amazon
+
+class Solution:
+    def maxVowelsSolutionON2(s, k):
+        pass
+
+
+s = "abciiidef"
+k = 3
+print(Solution.maxVowelsSolutionON2(s, k))
