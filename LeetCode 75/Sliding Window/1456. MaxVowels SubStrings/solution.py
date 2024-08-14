905b291729a14bb9d2177486aef7d315cf878e9c
Manan Arora <mananarora313@gmail.com>
2024-08-13 19:04:22 -0500

    added python solution in O(n2) and O(n)

--- LeetCode 75/Sliding Window/1456. MaxVowels SubStrings/solution.py
+++ LeetCode 75/Sliding Window/1456. MaxVowels SubStrings/solution.py
@@ -3,9 +3,41 @@
 
 class Solution:
     def maxVowelsSolutionON2(s, k):
-        pass
+        i = 0
+        j = k-1
+        maxCount = 0
+        while(j< len(s)):
+            count = 0
+            for k in s[i: j+1]:
+                if(k == 'a' or k == 'e' or k == 'i' or k == 'o' or k == 'u'):
+                    count += 1
+            maxCount = max(count, maxCount)
+            i += 1
+            j += 1
+        return maxCount
+
+    def maxVowelsSolutionSliding(s,k):
+        i = 0
+        j = k -1
+        count = 0
+        for z in s[i:j+1]:
+            if (z == 'a' or z == 'e' or z == 'i' or z == 'o' or z == 'u'):
+                count += 1
+        maxCount = count
+        i = 1
+        j = k
+        while(j < len(s)):
+            if(s[i-1] == 'a' or s[i-1] == 'e' or s[i-1] == 'i' or s[i-1] == 'o' or s[i-1] == 'u'):
+                count = count -1
+            if(s[j]== 'a' or s[j] == 'e' or s[j] == 'i' or s[j] == 'o' or s[j] == 'u'):
+                count = count + 1
+            maxCount = max(maxCount, count)
+            i += 1
+            j += 1
+        return maxCount
+
 
 
 s = "abciiidef"
 k = 3
-print(Solution.maxVowelsSolutionON2(s, k))
+print(Solution.maxVowelsSolutionSliding(s, k))
