0c8ca616605c5eb5c00237b5426c5c04483979b4
Manan Arora <mananarora313@gmail.com>
2024-08-15 12:29:39 -0500

    added python solution for 1493

---
+++ LeetCode 75/Sliding Window/1493. Longest SubArray of 1s after deleting 1 element/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestSubarray(nums):
+        i = 0
+        j = -1
+        count = 0
+        maxCount = 0
+        while i < len(nums):
+            if nums[i] == 1:
+                i += 1
+            else:
+                count += 1
+                i += 1
+            while(count >1):
+                j += 1
+                if(nums[j] == 0):
+                    count -= 1
+            
+            maxCount = max(maxCount, (i -j -1))
+        return maxCount -1
+
+
+nums = [0, 1, 1, 1, 0, 1, 1, 0, 1]
+print(Solution.longestSubarray(nums))
