793b051ab1be46bc82a4ed5b6c0acd9e77b536ec
Manan Arora <mananarora313@gmail.com>
2024-08-19 16:57:03 -0500

    added python solution for 242

---
+++ Neetcode 150/Arrays & Hashing/242. Valid Anagram/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(s, t):
+        dict = {}
+        count = 0
+        for i in s:
+            if(i not in dict):
+                dict[i] = 1
+            else:
+                dict[i] = dict.get(i) + 1
+        
+        for i in t:
+            if(i not in dict):
+                return False
+            else:
+                dict[i] = dict.get(i) - 1
+        
+        for i in dict.values():
+            if(i != 0):
+                return False
+        return True
+            
+
+s = "anagram"
+t = "nagaram"
+
+print(Solution.isAnagram(s,t))
