b80b82abc3949b5d50813345d9d9172595e11e41
Manan Arora <mananarora313@gmail.com>
2024-08-22 23:03:02 -0500

    Added 49 solution

--- Neetcode 150/Arrays & Hashing/49. Group Anagrams/groupAnagramsSol.java
+++ Neetcode 150/Arrays & Hashing/49. Group Anagrams/groupAnagramsSol.java
@@ -1,7 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class groupAnagramsSol {
 
-    // Do it tomorrow
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> newMap = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sortedChars = new String(chars);
+            if(!newMap.containsKey(sortedChars)){
+                newMap.put(sortedChars, new ArrayList<>());
+            }
+            newMap.get(sortedChars).add(s);
+        }
+
+        return new ArrayList<>(newMap.values());
+    }
+
     public static void main(String[] args) {
-        
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
     }
 }
