ebb7329b43e017abf5d3ac8e75842835ef159573
Manan Arora <mananarorauta@gmail.com>
2024-09-02 20:25:48 -0500

    added python solution for 155. Min Stack (#52)

---
+++ Neetcode 150/Stack/155. Min Stack/MinStack.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if(self.minStack):
+            self.minStack.append(min(val, self.minStack[-1]))
+        else:
+            self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+obj = MinStack()
+obj.push(10)
+obj.push(20)
+obj.pop()
+param_3 = obj.top()
+print(param_3)
+param_4 = obj.getMin()
