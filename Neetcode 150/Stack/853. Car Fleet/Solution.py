9182845c01bf26946a2ac0c14744610438e3aa32
manan05 <mananarorauta@gmail.com>
2024-09-05 17:01:16 -0500

    added python solution for 853 Car Fleet

---
+++ Neetcode 150/Stack/853. Car Fleet/Solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def carFleet(target, position, speed):
+        # pair = [[p, s] for p, s in zip(position, speed)]
+        
+        # stack = []
+        # for p,s in sorted(pair)[::-1]: # reverse sorted order
+        #     stack.append((target - p)/s)
+        #     if(len(stack) >= 2) and stack[-1]<= stack[-2]:
+        #         stack.pop()
+        # return len(stack)
+
+        pair = []
+        stack = []
+        for i in range(len(position)):
+            pair.append([position[i], speed[i]])
+        for p, s in sorted(pair)[::-1]:
+            time = target - p
+            stack.append(time/ s)
+            if(len(stack)>=2 and stack[-1]<= stack[-2]):
+                stack.pop()
+        return len(stack)
+
+
+
+
+target = 12
+position = [10,8,0,5,3]
+speed = [2,4,1,1,3]
+
+print(Solution.carFleet(target, position, speed=speed))
