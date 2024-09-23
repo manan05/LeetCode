c99a6d309a8095fcfd3c3469c050ea26ee4e4f73
Manan Arora <mananarora313@gmail.com>
2024-09-22 23:39:04 -0500

    updated solution for 33

--- Neetcode 150/Binary Search/33. Search in Rotated Sorted Array/solution.py
+++ Neetcode 150/Binary Search/33. Search in Rotated Sorted Array/solution.py
@@ -1,18 +1,6 @@
 class Solution:
     def searchRotatedArr(nums):
-        left  = 0
-        right = len(nums) - 1
-        if(nums[0] < nums[right]):
-            return nums[0]
-        while(left <= right):
-            mid = left + (right - left) // 2
-            if(nums[mid] > nums[mid + 1]):
-                return nums[mid + 1]
-            if(nums[mid] > nums[right]):
-                left = mid + 1
-            else:
-                right = mid - 1
-        return -1
+        
             
 arr = [3,4,5,1,2]
 print(Solution.searchRotatedArr(arr))
