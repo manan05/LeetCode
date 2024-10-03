2836e34a984f6fab5e6ff1b99d228517e22dbe88
Manan Arora <mananarorauta@gmail.com>
2024-10-02 20:08:43 -0500

    Time: 51 ms (77.25%), Space: 16.6 MB (83.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -8,5 +8,6 @@ class Solution:
                 resSet.remove(s[l])
                 l += 1
             resSet.add(s[r])
-            maxLen = max(maxLen, r-l + 1)
+            maxLen = max(maxLen, (r-l+1))
+
         return maxLen
