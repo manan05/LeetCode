07aa5b71c59ea85a835c57d42dabc31230c90356
Manan Arora <mananarorauta@gmail.com>
2024-10-14 18:11:35 -0500

    Time: 31 ms (88.75%), Space: 16.5 MB (31.67%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,17 +5,32 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        curr = ListNode(0) # default dummy node
-        curr.next = head
-        i = 0
-        first = head
-        while (first is not None):
-            i += 1
+        # but the below code is 2 pass meaning we have to move through the LL twice
+        # curr = ListNode(0) # default dummy node
+        # curr.next = head
+        # i = 0
+        # first = head
+        # while (first is not None):
+        #     i += 1
+        #     first = first.next
+        # i = i - n
+        # first = curr
+        # while i > 0:
+        #     i -= 1
+        #     first = first.next
+        # first.next = first.next.next
+        # return curr.next
+
+        # One pass algorithm
+        zeroth = ListNode(0)
+        zeroth.next = head
+        first, second = zeroth, zeroth
+        # move the first to n 
+        for _ in range(n + 1):
             first = first.next
-        i = i - n
-        first = curr
-        while i > 0:
-            i -= 1
+        
+        while first is not None:
             first = first.next
-        first.next = first.next.next
-        return curr.next
+            second = second.next
+        second.next = second.next.next
+        return zeroth.next
