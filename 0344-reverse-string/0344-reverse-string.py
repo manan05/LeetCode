829c4e1e43c0c3cca1c1be965f0a5aa7e99bb396
Manan Arora <mananarorauta@gmail.com>
2024-10-19 20:03:24 -0500

    Time: 7 ms (98.88%), Space: 27.2 MB (5.04%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,9 +4,22 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         # iteratively
+        # i = 0
+        # j = len(s) - 1
+        # while (i < j):
+        #     s[i], s[j] = s[j], s[i]
+        #     i += 1
+        #     j -= 1
+        
+        # recursively
+        def helper(i, j , s):
+            if(i < j):
+                s[i], s[j] = s[j], s[i]
+            else:
+                return
+            helper(i +1, j-1, s)
+
         i = 0
         j = len(s) - 1
-        while (i < j):
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
+        helper(i , j , s)
+        
