c8d10afae3bc1092b69086a8a3d1e2b7836fef85
Manan Arora <mananarorauta@gmail.com>
2024-11-24 12:52:20 -0600

    Time: 0 ms (100%), Space: 16.7 MB (41.78%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
@@ -1,10 +1,25 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
+        # O(N) - Linear solution
+        # for num in arr:
+        #     if (num <= k):
+        #         k += 1
+        #     elif (num > k):
+        #         break
+        # return k
 
-        for num in arr:
-            if (num <= k):
-                k += 1
-            elif (num > k):
-                break
-        return k
+        # because sorted, binary search!!
+        # O(logN)
+        left = 0
+        right = len(arr) - 1
+        while (left <= right):
+            pivot = (left + right) // 2
+            if(arr[pivot] - pivot - 1 < k):
+                left = pivot + 1
+            else:
+                right = pivot - 1
+        
+        return left + k
+
+        
         
