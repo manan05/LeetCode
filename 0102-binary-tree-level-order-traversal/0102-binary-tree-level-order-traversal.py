004304fefc8422d021a288cb59da1cab2b00ead3
Manan Arora <mananarorauta@gmail.com>
2025-01-08 04:31:11 +0530

    Time: 0 ms (100%), Space: 18.5 MB (22.61%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,16 +7,42 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None:
-            return []
-        res = []
-        def helper(node, level):
-            if len(res) == level:
-                res.append([])
-            res[level].append(node.val)
-            if node.left is not None:
-                helper(node.left, level + 1)
-            if node.right is not None:
-                helper(node.right, level + 1)
-        helper(root, 0)
-        return res
+        # # Recursive:
+        # if root is None:
+        #     return []
+        # res = []
+        # def helper(node, level):
+        #     if len(res) == level:
+        #         res.append([])
+        #     res[level].append(node.val)
+        #     if node.left is not None:
+        #         helper(node.left, level + 1)
+        #     if node.right is not None:
+        #         helper(node.right, level + 1)
+        # helper(root, 0)
+        # return res
+
+        # Iterative
+        levels = []
+        if not root:
+            return levels
+        level = 0
+        queue = deque(
+            [
+                root,
+            ]
+        )
+
+        while queue:
+            levels.append([])
+            length = len(queue)
+            for i in range(length):
+                nn = queue.popleft()
+                levels[level].append(nn.val)
+
+                if nn.left:
+                    queue.append(nn.left)
+                if nn.right:
+                    queue.append(nn.right)
+            level += 1
+        return levels
