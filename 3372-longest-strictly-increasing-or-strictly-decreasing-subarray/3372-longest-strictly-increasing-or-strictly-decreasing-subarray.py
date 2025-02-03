c85a55d6b63e5dd2d1aec0693616f192327922c5
Manan Arora <mananarorauta@gmail.com>
2025-02-03 12:43:22 -0600

    Time: 0 ms (100%), Space: 17.8 MB (47.33%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,22 +1,41 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
-        maxA = 0
+        # maxA = 0
         
-        for i in range(len(nums)):
-            c = 1
-            for j in range(i + 1, len(nums)):
-                if(nums[j] < nums[j-1]):
-                    c += 1
-                else:
-                    break
-            maxA = max(c, maxA)
+        # for i in range(len(nums)):
+        #     c = 1
+        #     for j in range(i + 1, len(nums)):
+        #         if(nums[j] < nums[j- 1]):
+        #             c += 1
+        #         else:
+        #             break
+        #     maxA = max(maxA, c)
         
-        for i in range(len(nums)):
-            c = 1
-            for j in range(i + 1, len(nums)):
-                if(nums[j] > nums[j - 1]):
-                    c += 1
-                else:
-                    break
-            maxA = max(c, maxA)
+        # for i in range(len(nums)):
+        #     c = 1
+        #     for j in range(i + 1, len(nums)):
+        #         if(nums[j] > nums[j - 1]):
+        #             c += 1
+        #         else:
+        #             break
+        #     maxA = max(c, maxA)
+        # return maxA
+
+        if len(nums) == 1:
+            return 1
+        
+        si = 1
+        sd = 1
+        maxA = 1
+        for i in range(len(nums) - 1):
+            if(nums[i] < nums[i + 1]):
+                si += 1
+                sd = 1
+            elif (nums[i] > nums[i + 1]):
+                sd += 1
+                si = 1
+            else:
+                si = 1
+                sd = 1
+            maxA = max(maxA, si, sd)
         return maxA
