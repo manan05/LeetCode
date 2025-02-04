c08c5c92aa0e0a8aa0513ac6b1f7f9c714a2535e
Manan Arora <mananarorauta@gmail.com>
2025-02-04 17:40:08 -0600

    Time: 0 ms (100%), Space: 17.9 MB (9.76%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -4,59 +4,34 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # # recursion
+        # # recursive
         # res = []
-        
-        # def preorder(node):
-        #     if node is None:
-        #         return
-        #     res.append(node.val)
-        #     preorder(node.left)
-        #     preorder(node.right)
-        
-        # preorder(root)
-        # return res
-        
-        # # using stack
         # if root is None:
-        #     return []
+        #     return res
 
-        # stack = [root,]
-        # res = []
-
-        # while stack:
-        #     curr = stack.pop()
+        # def helper(curr):
+        #     if curr is None:
+        #         return
         #     res.append(curr.val)
-        #     if(curr.right is not None):
-        #         stack.append(curr.right)
-        #     if(curr.left is not None):
-        #         stack.append(curr.left)
-        
+        #     helper(curr.left)
+        #     helper(curr.right)
+        # helper(root)
         # return res
 
-        # Morris Traversal
-        node = root
-        output = []
+        # iterative
+        if root is None:
+            return []
+        stack = [root,]
+        res = []
 
-        while node:
-            if not node.left:
-                output.append(node.val)
-                node = node.right
-            else:
-                pred = node.left
+        while stack:
+            curr = stack.pop()
+            res.append(curr.val)
+            if curr.right is not None:
+                stack.append(curr.right)
+            if curr.left is not None:
+                stack.append(curr.left)
 
-                while pred.right and pred.right is not node:
-                    pred = pred.right
-                
-                if not pred.right:
-                    output.append(node.val)
-                    pred.right = node
-                    node = node.left
-                else:
-                    pred.right = None
-                    node = node.right
-        return output
+        return res
