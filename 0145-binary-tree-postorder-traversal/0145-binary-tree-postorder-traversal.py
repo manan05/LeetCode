7fd53a8f05d8be1f540570a8a00958a715d97084
Manan Arora <mananarorauta@gmail.com>
2025-02-05 18:46:38 -0600

    Time: 0 ms (100%), Space: 17.6 MB (84.99%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -6,15 +6,59 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # # Approach 1: Recursive
+        # if root is None:
+        #     return []
+        # res = []
+
+        # def helper(curr):
+        #     if curr.left is not None:
+        #         helper(curr.left)
+        #     if curr.right is not None:
+        #         helper(curr.right)
+        #     res.append(curr.val)
+
+        # helper(root)
+        # return res
+
+        # # Approach 2: 2 Stack method
+        # res = []
+        # if root is None:
+        #     return res
+        
+        # pathStack = []
+        # mainStack = [root, ]
+
+        # while(mainStack):
+        #     curr = mainStack[-1]
+
+        #     if pathStack and pathStack[-1] == curr:
+        #         res.append(curr.val)
+        #         mainStack.pop()
+        #         pathStack.pop()
+        #     else:
+        #         pathStack.append(curr)
+        #         if curr.right is not None:
+        #             mainStack.append(curr.right)
+        #         if curr.left is not None:
+        #             mainStack.append(curr.left)
+        # return res
+
+        # Approach 3: Rearranging PreOrder
         if root is None:
             return []
         res = []
-
-        def postorder(node):
-            if node.left is not None:
-                postorder(node.left)
-            if node.right is not None:
-                postorder(node.right)
-            res.append(node.val)
-        postorder(root)
+        stack = [root, ]
+        while(stack):
+            curr = stack.pop()
+            res.append(curr.val)
+            if curr.left is not None:
+                stack.append(curr.left)
+            if curr.right is not None:
+                stack.append(curr.right)
+        res.reverse()
         return res
+
+
+
+        
