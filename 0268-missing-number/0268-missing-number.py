2d6127baad6e4027b02cec0b4133a6b7d689ad10
Manan Arora <mananarorauta@gmail.com>
2025-02-15 13:55:40 -0800

    Time: 0 ms (100%), Space: 18.6 MB (92.05%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -9,10 +9,18 @@ class Solution:
         # return i + 1
 
         # # HashMap (O (N))
-        myDict = {}
-        for i in range(len(nums)):
-            myDict[nums[i]] = 1
-        for i in range(len(myDict)):
-            if myDict.get(i) == None:
-                return i
-        return i + 1
+        # myDict = {}
+        # for i in range(len(nums)):
+        #     myDict[nums[i]] = 1
+        # for i in range(len(myDict)):
+        #     if myDict.get(i) == None:
+        #         return i
+        # return i + 1
+
+        # # Math Solution with O(1) - constant space
+        n = len(nums)
+        prod = (n*(n+1))//2
+        mySum = 0
+        for i in nums:
+            mySum += i
+        return prod - mySum
