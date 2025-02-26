7de11188d3739af121207dcd3e60d450b7cae594
Manan Arora <mananarorauta@gmail.com>
2025-02-25 20:30:55 -0800

    Time: 0 ms (100%), Space: 18.7 MB (19.37%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,19 +3,37 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        row = len(matrix)
-        col = len(matrix[0])
-        zeroes = []
-        for i in range(row):
-            for j in range(col):
+        # # Approach 1: using Array for extra space
+        # row = len(matrix)
+        # col = len(matrix[0])
+        # zeroes = []
+        # for i in range(row):
+        #     for j in range(col):
+        #         if matrix[i][j] == 0:
+        #             zeroes.append((i, j))
+        # for x, y in zeroes:
+        #     temp = 0
+        #     while(temp != row):
+        #         matrix[temp][y] = 0
+        #         temp += 1
+        #     temp = 0
+        #     while(temp != col):
+        #         matrix[x][temp] = 0
+        #         temp += 1
+
+        # # Approach 2: Using Set for extra space
+        m = len(matrix)
+        n = len(matrix[0])
+        rows = set()
+        cols = set()
+
+        for i in range(m):
+            for j in range(n):
                 if matrix[i][j] == 0:
-                    zeroes.append((i, j))
-        for x, y in zeroes:
-            temp = 0
-            while(temp != row):
-                matrix[temp][y] = 0
-                temp += 1
-            temp = 0
-            while(temp != col):
-                matrix[x][temp] = 0
-                temp += 1
+                    rows.add(i)
+                    cols.add(j)
+
+        for i in range(m):
+            for j in range(n):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
