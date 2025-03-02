836a232a22a09fe62503d22376b499082925159c
Manan Arora <mananarorauta@gmail.com>
2025-03-01 23:30:39 -0800

    Time: 0 ms (100%), Space: 17.9 MB (12.12%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,4 +3,38 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        # # O(N)
+        # countZero = 0
+        # countOne = 0
+        # countTwo = 0
+
+        # for i in nums:
+        #     if i == 0:
+        #         countZero += 1
+        #     elif i == 1:
+        #         countOne += 1
+        #     else:
+        #         countTwo += 1
+
+        # for i in range(countZero):
+        #     nums[i] = 0
+        # for i in range(countOne):
+        #     nums[i + countZero] = 1
+        # for i in range(countTwo):
+        #     nums[i + (countZero + countOne)] = 2
+
+        # # One Pass with constant memory
+
+        p0 = curr = 0
+        p2 = len(nums) - 1
+
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[p0], nums[curr] = nums[curr], nums[p0]
+                curr += 1
+                p0 += 1
+            elif nums[curr] == 2:
+                nums[p2], nums[curr] = nums[curr], nums[p2]
+                p2 -= 1
+            else:
+                curr += 1
