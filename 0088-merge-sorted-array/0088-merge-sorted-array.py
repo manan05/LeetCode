b7960aa620f56298ae40781d376a20b2d38454d4
Manan Arora <mananarorauta@gmail.com>
2025-03-07 10:26:57 -0800

    Time: 0 ms (100%), Space: 17.6 MB (99.28%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,7 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(n):
-            nums1[m + i] = nums2[i]
-        nums1.sort()
+        # # Approach 1: Brute Force
+        # for i in range(n):
+        #     nums1[m + i] = nums2[i]
+        # nums1.sort()
+
+        # # Approach 2: 
+        tempArr = nums1[:m]
+
+        p1 = 0
+        p2 = 0
+
+        p = 0
+        while (p < m + n):
+            if p2>= n or (p1 < m and (tempArr[p1] <= nums2[p2])):
+                nums1[p] = tempArr[p1]
+                p1 += 1
+            else:
+                nums1[p] = nums2[p2]
+                p2+= 1
+            p += 1
         
