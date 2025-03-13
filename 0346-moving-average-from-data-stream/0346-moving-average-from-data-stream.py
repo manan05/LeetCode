bf35362e9d6802108e5aa1ae3caab1c6d60de27c
Manan Arora <mananarorauta@gmail.com>
2025-03-13 11:11:12 -0700

    Time: 3 ms (83.09%), Space: 21.2 MB (9.16%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -1,14 +1,33 @@
+from collections import deque
 class MovingAverage:
 
-    def __init__(self, size: int):
-        self.size = size
-        self.nums = []
+    # # Approach 1: Using Array
+    # def __init__(self, size: int):
+    #     self.size = size
+    #     self.nums = []
 
-    def next(self, val: int) -> float:
-        nums = self.nums
-        nums.append(val)
-        sumN = sum(nums[-self.size:]) # sum of last (size)
-        return sumN/min(self.size, len(nums))
+    # def next(self, val: int) -> float:
+    #     # variables for size and nums arr
+    #     size = self.size
+    #     nums = self.nums
+    #     nums.append(val)
+    #     sumN = sum(nums[-size:])  # sum of last (size)
+    #     return sumN / min(size, len(nums))
+    # # Approach 2: Using a Queue
+    def __init__(self, size):
+        self.size = size
+        self.queue = deque()
+        self.window_sum = 0
+
+    def next(self, val):
+        self.queue.append(val)
+        if len(self.queue) > self.size:
+            elemPop = self.queue.popleft()
+        else:
+            elemPop = 0
+        self.window_sum = self.window_sum + val - elemPop
+        return self.window_sum / min(len(self.queue), self.size)
+
 
 
 # Your MovingAverage object will be instantiated and called as such:
