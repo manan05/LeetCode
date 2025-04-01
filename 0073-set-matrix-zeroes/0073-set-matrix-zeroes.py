533e98ccdcae6851cf28300edad03b0172d60193
Manan Arora <mananarorauta@gmail.com>
2025-04-01 12:09:00 -0700

    Time: 6 ms (23.77%), Space: 18.4 MB (55.39%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,62 +3,55 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # # Approach 1: using Array for extra space
-        # row = len(matrix)
-        # col = len(matrix[0])
-        # zeroes = []
-        # for i in range(row):
-        #     for j in range(col):
-        #         if matrix[i][j] == 0:
-        #             zeroes.append((i, j))
-        # for x, y in zeroes:
-        #     temp = 0
-        #     while(temp != row):
-        #         matrix[temp][y] = 0
-        #         temp += 1
-        #     temp = 0
-        #     while(temp != col):
-        #         matrix[x][temp] = 0
-        #         temp += 1
-
-        # # Approach 2: Using Set for extra space
+        # # Approach 1: Using Arr of tuples to store
+        # arr = []
         # m = len(matrix)
         # n = len(matrix[0])
+        # for i in range(m):
+        #     for j in range(n):
+        #         if matrix[i][j] == 0:
+        #             arr.append((i, j))
+        # for row, col in arr:
+        #     for i in range(n):
+        #         matrix[row][i] = 0
+        #     for j in range(m):
+        #         matrix[j][col] = 0
+
+        # # Approach 2: Using Set
         # rows = set()
         # cols = set()
-
+        # m = len(matrix)
+        # n = len(matrix[0])
         # for i in range(m):
         #     for j in range(n):
         #         if matrix[i][j] == 0:
         #             rows.add(i)
         #             cols.add(j)
-
         # for i in range(m):
         #     for j in range(n):
         #         if i in rows or j in cols:
         #             matrix[i][j] = 0
 
-        # Approach 3 - Constant Space
-        R = len(matrix)
-        C = len(matrix[0])
         is_col = False
-        for i in range(R):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        for i in range(m):
             if matrix[i][0] == 0:
                 is_col = True
-            for j in range(1, C):
+            for j in range(1, n):
                 if matrix[i][j] == 0:
-                    matrix[0][j] = 0
                     matrix[i][0] = 0
-
-        for i in range(1, R):
-            for j in range(1, C):
+                    matrix[0][j] = 0
+        for i in range(1, m):
+            for j in range(1, n):
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
-
+        
         if matrix[0][0] == 0:
-            for j in range(C):
-                matrix[0][j] = 0
-
-        if is_col == 1:
-            for i in range(R):
+            for i in range(n):
+                matrix[0][i] = 0
+        
+        if is_col:
+            for i in range(m):
                 matrix[i][0] = 0
