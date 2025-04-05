9a3eb182e66e426de43bf4d5826b8da4f79d85b9
Manan Arora <mananarorauta@gmail.com>
2025-04-05 13:36:43 -0700

    Time: 89 ms (30.97%), Space: 32.6 MB (51.85%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,27 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # # Approach 1: O(n3)
-        # if len(nums) == 1:
-        #     return nums[0]
-        # maxSum = - math.inf
-        # for i in range(len(nums)):
-        #     for j in range(i, len(nums)):
-        #         maxSum = max(maxSum, sum(nums[i:j+1]))
-        # return maxSum
-
-        # # Approach 2: O (N2) Optimized Brute Force
-        # maxSum = -math.inf
-        # for i in range(len(nums)):
-        #     curr_sum = 0
-        #     for j in range(i, len(nums)):
-        #         curr_sum += nums[j]
-        #         maxSum = max(curr_sum, maxSum)
-        # return maxSum
-
-        # # Approach 3: Kadane Algo : O(N) - Linear
-        maxSum = nums[0]
+        # # Kadanes (DP)
         currSum = nums[0]
-        for i in range(1, len(nums)):
-            currSum = max(nums[i], currSum + nums[i])
+        maxSum = nums[0]
+        for i in nums[1:]:
+            currSum = max(i, currSum + i)
             maxSum = max(maxSum, currSum)
         return maxSum
