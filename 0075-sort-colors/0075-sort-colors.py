4bdb1d759d783bd7935d6fc535acf051096e7640
Manan Arora <mananarorauta@gmail.com>
2025-04-05 14:49:36 -0700

    Time: 0 ms (100%), Space: 17.7 MB (53.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,38 +3,34 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # # O(N)
-        # countZero = 0
-        # countOne = 0
-        # countTwo = 0
-
+        # brute force using space
+        # hashmap = {}
         # for i in nums:
-        #     if i == 0:
-        #         countZero += 1
-        #     elif i == 1:
-        #         countOne += 1
+        #     hashmap[i] = hashmap.get(i, 0) + 1
+        # p0 = hashmap.get(0,0)
+        # p1 = p0 + hashmap.get(1, 0)
+
+        # for i in range(len(nums)):
+        #     if i < p0:
+        #         nums[i] = 0
+        #     elif (i >= p0 and i < p1):
+        #         nums[i] = 1
         #     else:
-        #         countTwo += 1
+        #         nums[i] = 2
 
-        # for i in range(countZero):
-        #     nums[i] = 0
-        # for i in range(countOne):
-        #     nums[i + countZero] = 1
-        # for i in range(countTwo):
-        #     nums[i + (countZero + countOne)] = 2
 
-        # # One Pass with constant memory
-
-        p0 = curr = 0
+        # 3 pointer approach
+        curr = 0
+        p0 = 0
         p2 = len(nums) - 1
-
         while curr <= p2:
             if nums[curr] == 0:
                 nums[p0], nums[curr] = nums[curr], nums[p0]
                 curr += 1
                 p0 += 1
             elif nums[curr] == 2:
-                nums[p2], nums[curr] = nums[curr], nums[p2]
+                nums[curr], nums[p2] = nums[p2], nums[curr]
                 p2 -= 1
             else:
                 curr += 1
+                
