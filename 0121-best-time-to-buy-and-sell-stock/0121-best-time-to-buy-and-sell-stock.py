f436211eb25c3b3690bf8e2394b8644e333756eb
Manan Arora <mananarorauta@gmail.com>
2025-04-07 13:52:43 -0700

    Time: 143 ms (12.26%), Space: 27 MB (9.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-    #    # Approach 1: Brute Force:
+        # Approach 1: Brute Force
+        # maxP = 0
+        # for i in range(len(prices)):
+        #     for j in range(i + 1, len(prices)):
+        #         profit = prices[j] - prices[i]
+        #         maxP = max(maxP, profit)
+        # return maxP
 
-    #     maxP = 0
-    #     for i in range(len(prices) - 1):
-    #         for j in range(i + 1, len(prices)):
-    #             profit = prices[j] - prices[i]
-    #             maxP = max(profit, maxP)
-    #     return maxP
-
-    # # Approach 2: One Pass Algo
-
-        curr = prices[0]
+        # Approach 2: DP
+        currP = prices[0]
         maxP = 0
-        for i in range(1, len(prices)):
-            curr = min(prices[i], curr)
-            maxP = max(prices[i] - curr, maxP)
+        for i in prices:
+            currP = min(i , currP)
+            maxP = max(maxP, i - currP)
         return maxP
