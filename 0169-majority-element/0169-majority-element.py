0561cb0ee49568e4aada62d8b99c0762eb57fcd0
Manan Arora <mananarorauta@gmail.com>
2025-04-14 14:54:46 -0700

    Time: 0 ms (100%), Space: 19.3 MB (52.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,35 @@
-from collections import Counter
+import random
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        c = {}
-        c = Counter(nums)
-        max_n = 0
-        for i in c.values():
-            max_n = max(max_n, i)
-        for i,j in c.items():
-            if j == max_n:
-                return i
+        # # Approach 1: Hashmap
+        # myDict = {}
+        # for i in nums:
+        #     myDict[i] = myDict.get(i, 0) + 1
+        # print(myDict)
         
+        # for i in myDict.items():
+        #     if i[1] > len(nums) // 2:
+        #         return i[0]
+
+        # # Approach 2: Sorting
+        # nums.sort()
+        # # majority element > n/2 
+        # return nums[len(nums) // 2]
+
+        # # Approach 3: Randomization
+        # while True:
+        #     candidate = random.choice(nums)
+        #     if sum(1 for elem in nums if elem == candidate) > len(nums) // 2:
+        #         return candidate
+
+        count = 0
+        candidate = None
+
+        for i in nums:
+            if count == 0:
+                candidate = i
+            if i == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
