6990f35088f0e31c299a694928b298c8300d927f
Manan Arora <mananarorauta@gmail.com>
2025-04-24 14:16:59 -0700

    Time: 3 ms (99.83%), Space: 18.2 MB (5.64%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -1,12 +1,28 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
+        # # Brute Force Solution
+        # k = len(set(nums))
+        # res = 0
+
+        # for i in range(len(nums)):
+        #     mSet = set()
+        #     for j in range(i, len(nums)):
+        #         mSet.add(nums[j])
+        #         if len(mSet) == k:
+        #             res += 1
+        # return res
+
+        # # Sliding Window approach
         k = len(set(nums))
         res = 0
-
+        left = 0
+        hashmap = {}
         for i in range(len(nums)):
-            mSet = set()
-            for j in range(i, len(nums)):
-                mSet.add(nums[j])
-                if len(mSet) == k:
-                    res += 1
+            hashmap[nums[i]] = hashmap.get(nums[i], 0) + 1
+            while len(hashmap) == k:
+                res += len(nums) - i
+                hashmap[nums[left]] = hashmap.get(nums[left]) - 1
+                if hashmap[nums[left]] == 0:
+                    del hashmap[nums[left]]
+                left += 1
         return res
