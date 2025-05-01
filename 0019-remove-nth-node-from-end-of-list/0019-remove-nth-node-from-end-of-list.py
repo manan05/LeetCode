87e78d7adfc18f787a429a7cde17e5dcd24ae2d9
Manan Arora <mananarorauta@gmail.com>
2025-05-01 14:12:17 -0700

    Time: 0 ms (100%), Space: 17.9 MB (34.57%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,32 +5,34 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # but the below code is 2 pass meaning we have to move through the LL twice
-        # curr = ListNode(0) # default dummy node
-        # curr.next = head
-        # i = 0
-        # first = head
-        # while (first is not None):
-        #     i += 1
-        #     first = first.next
-        # i = i - n
-        # first = curr
-        # while i > 0:
-        #     i -= 1
-        #     first = first.next
-        # first.next = first.next.next
-        # return curr.next
+        # # 2 Pass algo
+        # size = 0
+        # prev = ListNode(0)
+        # prev.next = head
+        # curr = head
+        # while curr:
+        #     size += 1
+        #     curr = curr.next
+        # idx = size - n
+        # curr = prev
+        # while curr:
+        #     if idx == 0:
+        #         curr.next = curr.next.next
+        #     else:
+        #         curr = curr.next
+        #     idx -= 1
+        # return prev.next
 
-        # One pass algorithm
-        zeroth = ListNode(0)
-        zeroth.next = head
-        first, second = zeroth, zeroth
-        # move the first to n 
-        for _ in range(n + 1):
-            first = first.next
-        
-        while first is not None:
-            first = first.next
-            second = second.next
+        # # One Pass
+        prevNode = ListNode(0)
+        prevNode.next = head
+        first, second = prevNode, prevNode
+        while first:
+            if n >= 0:
+                first = first.next
+            else:
+                first = first.next
+                second = second.next
+            n -= 1
         second.next = second.next.next
-        return zeroth.next
+        return prevNode.next
