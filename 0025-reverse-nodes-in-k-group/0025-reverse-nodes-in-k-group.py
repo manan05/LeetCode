ae328471040f9dc40bfedd7eeb2924dd1fc91127
Manan Arora <mananarorauta@gmail.com>
2025-05-03 17:02:13 -0700

    Time: 0 ms (100%), Space: 18.6 MB (60.35%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,8 +5,33 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # # Approach 1: Recursion
-        def reverseLinkList(head, k):
+        # # Approach 1: Using Space and recursion``
+        # def reverseLinkList(head, k):
+        #     prev = None
+        #     curr = head
+        #     while k > 0:
+        #         temp = curr.next
+        #         curr.next = prev
+        #         prev = curr
+        #         curr = temp
+        #         k -= 1
+        #     return prev
+
+        # i = 0
+        # curr = head
+        # while i < k and curr:
+        #     curr = curr.next
+        #     i += 1
+        # if i == k:
+        #     # reverse the current group
+        #     reversedHead = reverseLinkList(head, k)
+
+        #     # recursively reverse the other as well
+        #     head.next = self.reverseKGroup(curr, k)
+        #     return reversedHead
+        # return head
+
+        def reverseLL(head, k):
             prev = None
             curr = head
             while k:
@@ -14,17 +39,26 @@ class Solution:
                 curr.next = prev
                 prev = curr
                 curr = temp
-
                 k -= 1
             return prev
-        
-        i = 0
+
         curr = head
-        while i < k and curr:
-            curr = curr.next
-            i += 1
-        if i == k:
-            reversedHead = reverseLinkList(head, k)
-            head.next = self.reverseKGroup(curr, k)
-            return reversedHead
-        return head
+        tail = None
+        new_head = None
+        while curr:
+            count = 0
+            while count < k and curr:
+                curr = curr.next
+                count += 1
+            if count == k:
+                reversedHead = reverseLL(head, k)
+                if not new_head:
+                    new_head = reversedHead
+                if tail:
+                    tail.next = reversedHead
+                tail = head
+                head = curr
+        if tail:
+            tail.next = head
+
+        return new_head if new_head else head
