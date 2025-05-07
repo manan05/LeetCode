92e5de334e25657f43d11222b56e1da543ca9361
Manan Arora <mananarorauta@gmail.com>
2025-05-07 16:18:23 -0500

    Time: 34 ms (92.51%), Space: 18.6 MB (40.92%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,19 +9,48 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # # Approach 1: Using extra space - Hashmap
+        # hashmap = {}
+        # if not head:
+        #     return None
+        # curr = head
+        # while curr:
+        #     hashmap[curr] = Node(curr.val)
+        #     curr = curr.next
+
+        # temp = head
+        # while temp:
+        #     hashmap[temp].next = hashmap.get(temp.next)
+        #     hashmap[temp].random = hashmap.get(temp.random)
+        #     temp = temp.next
+
+        # return hashmap[head]
+
         if not head:
             return None
 
-        hashmap = {}
+        # New nodes create
         curr = head
         while curr:
-            hashmap[curr] = Node(curr.val)
-            curr = curr.next
+            new_node = Node(curr.val)
+            new_node.next = curr.next
+            curr.next = new_node
+            curr = new_node.next
 
-        # Next and random pointers
+        # Assign random pointers
         curr = head
         while curr:
-            hashmap[curr].next = hashmap.get(curr.next)
-            hashmap[curr].random = hashmap.get(curr.random)
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+
+        # Remove the links from the og LL
+        curr = head
+        new_head = head.next
+        while curr:
+            copy = curr.next
+            curr.next = copy.next
+            if copy.next:
+                copy.next = copy.next.next
             curr = curr.next
-        return hashmap[head]
+        return new_head
