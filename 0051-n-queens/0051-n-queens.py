a9addecd7896964e8c0e2995bd143b396bf641d9
Manan Arora <mananarorauta@gmail.com>
2025-05-30 17:23:27 -0500

    Time: 43 ms (16.71%), Space: 18.1 MB (96.29%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -2,27 +2,24 @@ class Solution:
     def isSafe1(self, row, col, board, n):
         tempRow = row
         tempCol = col
-        
-        # # Upper left diagonal
+        # # upper left diagonal
         while row >= 0 and col >= 0:
             if board[row][col] == 'Q':
                 return False
             row -= 1
             col -= 1
 
-        # # Same Row
+        # # same row - left side
         row = tempRow
         col = tempCol
-
-        while row < n and col >= 0:
+        while col >= 0:
             if board[row][col] == 'Q':
                 return False
             col -= 1
 
-        # # Lower Left diagonal
+        # # bottom left diagonal
         row = tempRow
         col = tempCol
-
         while row < n and col >= 0:
             if board[row][col] == 'Q':
                 return False
@@ -31,19 +28,19 @@ class Solution:
 
         return True
 
-    def solve(self, col, board, ans, n):
+    def solve(self, col, board, res, n):
         if col == n:
-            ans.append(board[:])
+            res.append(board[:])
             return
-
+        
         for row in range(n):
             if self.isSafe1(row, col, board, n):
                 board[row] = board[row][:col] + 'Q' + board[row][col + 1:]
-                self.solve(col + 1, board, ans, n)
+                self.solve(col + 1, board, res, n)
                 board[row] = board[row][:col] + '.' + board[row][col + 1:]
 
     def solveNQueens(self, n: int) -> List[List[str]]:
-        ans = []
-        board = ['.' * n for _ in range(n)]
-        self.solve(0, board, ans, n)
-        return ans
+        board = ["." * n for _ in range(n)]
+        res = []
+        self.solve(0, board, res, n)
+        return res
