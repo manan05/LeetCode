14a2e61cbe3534451797bf921fcb87da44c38f35
Manan Arora <mananarorauta@gmail.com>
2025-06-04 15:33:23 -0500

    Time: 0 ms (100%), Space: 17.7 MB (91.68%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,20 +1,19 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        def helper(start, s, curr, ans, wordDict):
+        def helper(start, curr, s, wordDict, ans):
             if start == len(s):
                 ans.append(curr)
                 return
-
             for end in range(start + 1, len(s) + 1):
                 mStr = s[start:end]
                 if mStr in wordDict:
                     temp = curr
                     if curr:
-                        curr = curr + " "
+                        curr += " "
                     curr += mStr
-                    helper(end, s, curr, ans, wordDict)
+                    helper(end, curr, s, wordDict, ans)
                     curr = temp
 
         ans = []
-        helper(0, s, "", ans, wordDict)
+        helper(0, "", s, wordDict, ans)
         return ans
