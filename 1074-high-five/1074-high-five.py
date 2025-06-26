b40e233312964d16878fc797831edc3a545d4cf0
Manan Arora <mananarorauta@gmail.com>
2025-06-26 01:25:59 -0500

    Time: 0 ms (100%), Space: 17.8 MB (97.01%) - LeetHub

--- 1074-high-five/1074-high-five.py
+++ 1074-high-five/1074-high-five.py
@@ -1,25 +1,11 @@
-import heapq
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
-        # # Sorting:
-        # mapping = defaultdict(list)
-        # items.sort(key=lambda x: (x[0], -x[1]))
-        # for student, score in items:
-        #     mapping[student].append(score)
-        # res = []
-        # for id in mapping:
-        #     topFive = mapping[id][:5]
-        #     res.append([id, sum(topFive) // 5])
-        # return res
-
-        # # Using Heap
-        mapping = defaultdict(list)
-        for student, score in items:
-            heapq.heappush(mapping[student], score)
-            if len(mapping[student]) > 5:
-                heapq.heappop(mapping[student])
-
+        hashmap = defaultdict(list)
+        for st, marks in items:
+            heapq.heappush(hashmap[st], marks)
+            if len(hashmap[st]) > 5:
+                heapq.heappop(hashmap[st])
         res = []
-        for id in mapping:
-            res.append([id, sum(mapping[id]) // 5])
+        for st, marks in hashmap.items():
+            res.append([st, sum(marks) // 5])
         return sorted(res)
