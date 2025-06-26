5417e0016ba5c4e9bca76990cd81a65b8258db53
Manan Arora <mananarorauta@gmail.com>
2025-06-26 15:09:20 -0500

    Time: 0 ms (100%), Space: 19 MB (33.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,26 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # # Approach 1: Brute Force - 2 loops
-        # for i in range(len(nums) - 1):
-        #     for j in range(1, len(nums)):
-        #         if (nums[i] + nums[j]) == target:
-        #             return [i, j]
-
-        # # Approach 2: 2 pass Hashmaps
-        # myDict = {}
-        # for i in range(len(nums)):
-        #     myDict[nums[i]] = i 
-
-        # for i in range(len(nums)):
-        #     complement = target - nums[i]
-        #     if complement in myDict and myDict[complement] != i:
-        #         return [i, myDict[complement]]
-
-        # # Approach 3: 1 pass Hashmap
-        myDict = {}
+        hashmap = {}
         for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in myDict and myDict[complement] != i:
-                return [i, myDict[complement]]
-            else:
-                myDict[nums[i]] = i
+            diff = target - nums[i]
+            if diff in hashmap and hashmap[diff] != i:
+                return [i, hashmap[diff]]
+            hashmap[nums[i]] = i
+        return -1
