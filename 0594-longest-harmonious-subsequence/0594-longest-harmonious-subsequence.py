07f4425a86e35efc1229b0999e3f46f40a5f2210
Manan Arora <mananarorauta@gmail.com>
2025-06-30 16:45:52 -0500

    Time: 18 ms (91.33%), Space: 19.3 MB (22%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -1,12 +1,18 @@
-from collections import Counter
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        HashMap = Counter(nums)
-        maxCount = 0
-        
-        for key in HashMap.keys():
-            count = 0
-            if(HashMap[key +1]):
-                count += HashMap.get(key) + HashMap.get(key + 1)
-            maxCount = max(maxCount, count)
-        return maxCount
+        # # Using sorting (ONlogN)
+        # counter = Counter(nums)
+        # nums.sort()
+        # maxL = 0
+        # for i in range(1, len(nums)):
+        #     if nums[i] - nums[i - 1] == 1:
+        #         maxL = max(maxL, counter[nums[i]] + counter[nums[i - 1]])
+        # return maxL
+
+        # # Using only Hashmap
+        counter = Counter(nums)
+        maxL = 0
+        for key in counter.keys():
+            if key + 1 in counter:
+                maxL = max(maxL, counter[key + 1] + counter[key])
+        return maxL
