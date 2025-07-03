234f4016c5774a91d910543da6a81e2e9afc644d
Manan Arora <mananarorauta@gmail.com>
2025-07-02 20:46:05 -0500

    Time: 7 ms (88.4%), Space: 19.4 MB (38.5%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,40 +1,22 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # # Brute Force:
+        # # Brute Force
         # ans = 0
-        # n = len(height)
-        # for i in range(1, n - 1):
+        # for i in range(1, len(height) - 1):
         #     left_max = 0
         #     right_max = 0
         #     for j in range(i, -1, -1):
         #         left_max = max(left_max, height[j])
-        #     for j in range(i, n):
+        #     for j in range(i, len(height)):
         #         right_max = max(right_max, height[j])
         #     ans += min(left_max, right_max) - height[i]
-        # return ans    
-
-        # # Approach 2: DP
-        # ans = 0
-        # n = len(height)
-        # left_max = [0] * n
-        # right_max = [0] * n
-
-        # left_max[0] = height[0]
-        # for i in range(1, n):
-        #     left_max[i] = max(height[i], left_max[i - 1])
-
-        # right_max[n - 1] = height[n - 1]
-        # for i in range(n - 2, -1, -1):
-        #     right_max[i] = max(height[i], right_max[i + 1])
-
-        # for i in range(1, n - 1):
-        #     ans += min(left_max[i], right_max[i]) - height[i]
-
         # return ans
 
-        # # Approach 3: Two Pointer Approach
-        left, right = 0, len(height) - 1
-        left_max, right_max = 0, 0
+        # # Optimized Solution
+        left = 0
+        right = len(height) - 1
+        left_max = 0
+        right_max = 0
         ans = 0
         while left < right:
             if height[left] < height[right]:
@@ -46,3 +28,4 @@ class Solution:
                 ans += right_max - height[right]
                 right -= 1
         return ans
+
