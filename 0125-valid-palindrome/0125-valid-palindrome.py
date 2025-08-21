ad05883f1d797a37da19c12d98b146db432658d8
Manan Arora <mananarorauta@gmail.com>
2025-08-21 18:15:11 -0500

    Time: 7 ms (81.26%), Space: 18.1 MB (68.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,33 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # # iterative, two pointer
-        # lower_str = s.lower()
-        # cut_str = []
-        # for i in lower_str:
-        #     if i.isalnum():
-        #         cut_str.append(i)
-        # l = 0
-        # r = len(cut_str) - 1
-        # while l < r:
-        #     if cut_str[l] != cut_str[r]:
-        #         return False
-        #     l += 1
-        #     r -= 1
-        # return True
-
-        # recursion answer
-
-        myStr = [ch for ch in s.lower() if ch.isalnum()]
-        n = len(myStr) - 1
-
-        def helper(i, n, myStr):
-            if n < i:
-                return True
-
-            # self work
-            if (myStr[i] != myStr[n]):
+        lower_str = s.lower()
+        cut_str = ""
+        for i in lower_str:
+            if i.isalnum():
+                cut_str += i
+        left = 0
+        right = len(cut_str) - 1
+        while left < right:
+            if cut_str[left] != cut_str[right]:
                 return False
-
-            return helper(i + 1, n - 1, myStr)
-
-        return helper(0, n, myStr)
+            left += 1
+            right -= 1
+        return True
