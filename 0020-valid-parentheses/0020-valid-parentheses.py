7e69123acd11c413344199831cafca9a2aded074
Manan Arora <mananarorauta@gmail.com>
2025-08-21 19:44:59 -0500

    Time: 0 ms (100%), Space: 17.7 MB (80.74%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,12 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        mDict = {')': '(', ']':'[', '}':'{'}
-        for char in s:
-            if char in mDict:
-                top = stack.pop() if stack else '#'
-                if mDict[char] != top:
-                    return False
+        mDict = {'}': '{', ']': '[', ')': '('}
+        for i in s:
+            if i not in mDict:
+                stack.append(i)
             else:
-                stack.append(char)
-        return True if not stack else False
+                if stack and stack[-1] == mDict[i]:
+                    stack.pop(-1)
+                else:
+                    return False
+        return True if len(stack) == 0 else False
