3fb434629eca81d1f54913a9a2614c8353e4c788
Manan Arora <mananarorauta@gmail.com>
2025-08-23 21:54:35 -0500

    Time: 0 ms (100%), Space: 17.8 MB (54.7%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,34 +5,17 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # # 2 Pass algo
-        # size = 0
-        # prev = ListNode(0)
-        # prev.next = head
-        # curr = head
-        # while curr:
-        #     size += 1
-        #     curr = curr.next
-        # idx = size - n
-        # curr = prev
-        # while curr:
-        #     if idx == 0:
-        #         curr.next = curr.next.next
-        #     else:
-        #         curr = curr.next
-        #     idx -= 1
-        # return prev.next
-
-        # # One Pass
-        prevNode = ListNode(0)
-        prevNode.next = head
-        first, second = prevNode, prevNode
-        while first:
-            if n >= 0:
-                first = first.next
-            else:
-                first = first.next
-                second = second.next
-            n -= 1
-        second.next = second.next.next
-        return prevNode.next
+        dummy = ListNode(0)
+        dummy.next = head
+        curr = head
+        length = 0
+        while curr:
+            length += 1
+            curr = curr.next
+        length -= n
+        curr = dummy
+        while length != 0:
+            curr = curr.next
+            length -= 1
+        curr.next = curr.next.next
+        return dummy.next
