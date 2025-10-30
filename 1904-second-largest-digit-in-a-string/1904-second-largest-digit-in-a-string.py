6f5a0f2922f65a1b687b21ad4472ed4fe9f71900
Manan Arora <mananarorauta@gmail.com>
2025-10-30 04:16:18 -0500

    Time: 0 ms (100%), Space: 17.8 MB (51.51%) - LeetHub

--- 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
@@ -4,5 +4,5 @@ class Solution:
         for i in s:
             if i.isdigit():
                 res.add(i)
-        res = sorted(res)
+        res = sorted(res)   # O(1)
         return int(res[-2]) if len(res) >= 2 else -1
