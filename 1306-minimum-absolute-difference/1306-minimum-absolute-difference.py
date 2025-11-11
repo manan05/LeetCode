8d3e7646602e256706ee46c46d6ef30ddb51ea52
Manan Arora <mananarorauta@gmail.com>
2025-11-11 13:48:20 -0600

    Time: 43 ms (94.5%), Space: 30 MB (74.47%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.py
@@ -1,39 +1,38 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        # res = []
-        # arr.sort()
-        # minDiff = float('inf')
-        # n = len(arr)
-        # for i in range(n - 1):
-        #     currDiff = arr[i + 1] - arr[i]
-        #     minDiff = min(minDiff, currDiff)
-        # for i in range(n - 1):
-        #     currDiff = arr[i + 1] - arr[i]
-        #     if currDiff == minDiff:
-        #         res.append([arr[i], arr[i + 1]])
-        # return res
-
-        # # Count sort approach
-        min_val = min(arr)
-        max_val = max(arr)
-
-        offset = -min_val
-        freq = [0] * (max_val - min_val + 1)
-
-        for num in arr:
-            freq[num + offset] = 1
-
-        prev = None
-        minDiff = float('inf')
         res = []
-        for i in range(len(freq)):
-            if freq[i] == 1:
-                if prev is not None:
-                    currDiff = i - prev
-                    if currDiff < minDiff:
-                        res = [[prev - offset, i - offset]]
-                        minDiff = currDiff
-                    elif currDiff == minDiff:
-                        res.append([prev - offset, i - offset])
-                prev = i
+        minDiff = float('inf')
+        arr.sort()
+        for i in range(len(arr) - 1):
+            currDiff = arr[i + 1] - arr[i]
+            if currDiff < minDiff:
+                res = [[arr[i], arr[i + 1]]]
+                minDiff = currDiff
+            elif currDiff == minDiff:
+                res.append([arr[i], arr[i + 1]])
         return res
+
+        # # # Count sort approach
+        # min_val = min(arr)
+        # max_val = max(arr)
+
+        # offset = -min_val
+        # freq = [0] * (max_val - min_val + 1)
+
+        # for num in arr:
+        #     freq[num + offset] = 1
+
+        # prev = None
+        # minDiff = float('inf')
+        # res = []
+        # for i in range(len(freq)):
+        #     if freq[i] == 1:
+        #         if prev is not None:
+        #             currDiff = i - prev
+        #             if currDiff < minDiff:
+        #                 res = [[prev - offset, i - offset]]
+        #                 minDiff = currDiff
+        #             elif currDiff == minDiff:
+        #                 res.append([prev - offset, i - offset])
+        #         prev = i
+        # return res
