81e891b4bfacd003744c208a8161d9105e823341
Manan Arora <mananarorauta@gmail.com>
2025-11-20 22:37:27 -0600

    Time: 9 ms (36.88%), Space: 18.8 MB (10.13%) - LeetHub

--- 0860-design-circular-queue/0860-design-circular-queue.py
+++ 0860-design-circular-queue/0860-design-circular-queue.py
@@ -4,35 +4,40 @@ class ListNode:
         self.next = nxt
         self.prev = prev
 
-
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.space = k 
+        self.space = k
         self.left = ListNode(-1, None, None)
         self.right = ListNode(-1, None, self.left)
         self.left.next = self.right
 
+
     def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
-        curr = ListNode(value, None, None)
-        last_elem = self.right.prev
-        last_elem.next = curr
-        self.right.prev = curr
-        curr.next = self.right
-        curr.prev = last_elem
-        
+
+        elem_to_add = ListNode(value, None, None)
+        prev_elem = self.right.prev
+        self.right.prev = elem_to_add
+        prev_elem.next = elem_to_add
+
+        elem_to_add.prev = prev_elem
+        elem_to_add.next = self.right
         self.space -= 1
-        return True  
+        return True
+
 
     def deQueue(self) -> bool:
         if self.isEmpty():
             return False
+
         self.left.next = self.left.next.next
         self.left.next.prev = self.left
+
         self.space += 1
         return True
+        
 
     def Front(self) -> int:
         if self.isEmpty():
@@ -49,7 +54,6 @@ class MyCircularQueue:
 
     def isFull(self) -> bool:
         return self.space == 0
-
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
