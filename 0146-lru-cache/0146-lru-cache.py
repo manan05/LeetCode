6137d6097a2a6aa52ef4665b0d40f45f00348f0c
Manan Arora <mananarorauta@gmail.com>
2025-11-21 08:33:22 -0600

    Time: 129 ms (66.4%), Space: 77.8 MB (91.4%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,9 +1,9 @@
 class ListNode:
     def __init__(self, key, val):
         self.key = key
-        self.val = val
-        self.prev = None
+        self.value = val
         self.next = None
+        self.prev = None
 
 class LRUCache:
 
@@ -15,7 +15,6 @@ class LRUCache:
 
         self.left.next = self.right
         self.right.prev = self.left
-        
 
     def get(self, key: int) -> int:
         if key not in self.cache:
@@ -23,30 +22,30 @@ class LRUCache:
         node = self.cache[key]
         self.remove(node)
         self.insert(node)
-        return node.val
+        return node.value
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
         curr = ListNode(key, value)
-        self.cache[key] = curr
         self.insert(curr)
+        self.cache[key] = curr
 
         if len(self.cache) > self.capacity:
-            node_to_be_del = self.left.next
-            self.remove(node_to_be_del)
-            del self.cache[node_to_be_del.key]
+            node_to_del = self.left.next
+            self.remove(node_to_del)
+            del self.cache[node_to_del.key]
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
 
     def insert(self, node):
         prev_end = self.right.prev
         prev_end.next = node
         self.right.prev = node
-        node.next = self.right
         node.prev = prev_end
-    
-    def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
+        node.next = self.right
 
 
 # Your LRUCache object will be instantiated and called as such:
