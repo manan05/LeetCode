36a5f79457e194a48561b956bb6438ddf935817e
Manan Arora <mananarorauta@gmail.com>
2025-11-21 01:24:13 -0600

    Time: 11 ms (59.3%), Space: 24.8 MB (37.85%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.py
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -13,6 +13,7 @@ class Logger:
         self.log[message] = timestamp
         return True
 
+
 # Your Logger object will be instantiated and called as such:
 # obj = Logger()
 # param_1 = obj.shouldPrintMessage(timestamp,message)
