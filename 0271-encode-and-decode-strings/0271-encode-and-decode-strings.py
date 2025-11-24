3f1c8405d72994e1dfc9ee8ba1a560c714170f96
Manan Arora <mananarorauta@gmail.com>
2025-11-24 17:12:41 -0600

    Time: 216 ms (5.09%), Space: 18 MB (80.5%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -10,21 +10,21 @@ class Codec:
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        decoded_strings = []
-        curr_string = ""
+        res = []
+        curr_str = ''
         i = 0
         while i < len(s):
             if s[i: i + 2] == '/:':
-                decoded_strings.append(curr_string)
-                curr_string = ''
+                res.append(curr_str)
+                curr_str = ''
                 i += 2
             elif s[i: i + 2] == '//':
-                curr_string += '/'
+                curr_str += '/'
                 i += 2
             else:
-                curr_string += s[i]
+                curr_str += s[i]
                 i += 1
-        return decoded_strings
+        return res
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
